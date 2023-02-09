aedf66b82bbd4f94648bdac62df10d6f615d4529
Rabah Ghodbane <r-ghodbane@live.com>
2023-02-09 09:54:19 +0100

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import './globals.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={`${inter.variable}`}>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
